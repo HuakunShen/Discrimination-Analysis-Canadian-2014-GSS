e615f730f8f3585432597d24fb52bf7a5e9d2d18
Huakun Shen <huakun.shen@huakunshen.com>
2022-03-20 11:02:55 -0400

    clean up my code

--- scripts/constants.R
+++ scripts/constants.R
@@ -3,4 +3,3 @@ msvic_num <- c(101, 202, 203, 304, 405, 406, 507, 508, 509, 609, 610, 711, 712, 
 msvic_labels <- c("Sexual assault", "Robbery", "Attempted robbery", "Assault", "Break and enter", "Attempted break and enter", "Motor vehicle theft", "Part of a motor vehicle theft", "Attempted motor vehicle (parts) theft", "Theft of personal property", "Attempted theft of personal property", "Theft of household property", "Attempted theft of household property", "Vandalism", "Not asked - no crime reported", "Unclassifiable", "Valid skip", "Don't know", "Refusal", "Not stated")
 msvic_num_2_label <- hash(msvic_num, msvic_labels)
 
-names(msvic_num)=msvic_labels
--- scripts/huakun.R
+++ scripts/huakun.R
@@ -5,15 +5,7 @@ library(kableExtra)
 library(here)
 library(hash)
 
-gss_data <-
-  read_csv(here::here("inputs/data/data.csv"))
-glimpse(gss_data)
-
-colnames(gss_data)
-
-count(gss_data, msvic, dis_10)
-
-count(gss_data, dis_10)
+gss_data <- read_csv(here::here("inputs/data/data.csv"))
 
 bool_only_data = gss_data %>% filter(dis_10 < 3)
 
@@ -26,8 +18,6 @@ count_df <- rename(merge(x=count_df, y=sum_freq_by_msvic, by="msvic", all = TRUE
 
 count_df$perc = count_df$freq / count_df$total
 
-count_df
-
 # map dis_10 column to English
 dis_10_unique=sort(unique(count_df$dis_10))
 dis_10_en = c("No", "Yes")
@@ -36,31 +26,16 @@ count_df$dis_10_en=dis_10_en[count_df$dis_10]
 
 # map msvic column to English
 source(here::here("scripts/constants.R"))
-count_df$msvic_en=msvic_labels[count_df$msvic]
-msvic_unique
-count_df
-
 count_df$msvic_en = ""
 for(i in 1:nrow(count_df)) {
   row <- count_df[i,]
   count_df[i,]$msvic_en <- msvic_num_2_label[[as.character(row$msvic)]]
 }
 
-filter(count_df, dis_10==1)
-
-filter(count_df, dis_10==2)
-
-count_df <- count_df %>% mutate(dis_10_en=dis_10)
-
-g <- ggplot(count_df, aes(x=msvic_en, y=perc)) + geom_bar()
-g
-
-ggplot(count_df, aes(x=msvic_en, y=perc)) + geom_bar()
-
 x = c("dis_10_en")
 bar_plot <- ggplot(count_df, aes(x = msvic_en, y = perc, fill=.data[["dis_10_en"]])) + geom_bar(stat = "identity") + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-
+bar_plot
 
 
