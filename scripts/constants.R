d06f63b32bf7a4b7da078e8ae5432393f76aa5b6
Huakun Shen <huakun.shen@huakunshen.com>
2022-03-21 17:24:37 -0400

    Remove hash library and add figure captions

--- scripts/constants.R
+++ scripts/constants.R
@@ -1,13 +1,11 @@
-library(hash)
 msvic_num <- c(101, 202, 203, 304, 405, 406, 507, 508, 509, 609, 610, 711, 712, 813, 994, 995, 996, 997, 998, 999)
 msvic_labels <- c("Sexual assault", "Robbery", "Attempted robbery", "Assault", "Break and enter", "Attempted break and enter", "Motor vehicle theft", "Part of a motor vehicle theft", "Attempted motor vehicle (parts) theft", "Theft of personal property", "Attempted theft of personal property", "Theft of household property", "Attempted theft of household property", "Vandalism", "Not asked - no crime reported", "Unclassifiable", "Valid skip", "Don't know", "Refusal", "Not stated")
-msvic_num_2_label <- hash(msvic_num, msvic_labels)
+
 
 eduf10 <- c()
 
 eduf10_num <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 96, 97, 98, 99)
 eduf10_labels <- c("Doctorate/masters/some graduate", "Bachelor's degree", "Diploma/certificate from community college", "Diploma/certificate from trade/technical", "Some university", "Some community college/CEGEP/nursing", "Some trade/technical", "High school diploma", "Some secondary/high school", "Elementary school/no school", "Valid skip", "Don't know", "Refusal", "Not stated")
-eduf10_num_2_label <- hash(eduf10_num, eduf10_labels)
 
 
 dis_10_num <- c(1, 2, 6, 7, 8, 9)
