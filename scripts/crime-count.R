cc57f08509f5cdccf5b047f9e50f1158567864b3
Huakun Shen <huakun.shen@huakunshen.com>
2022-03-21 16:08:36 -0400

    Add results for crime count

--- scripts/crime-count.R
+++ scripts/crime-count.R
@@ -8,12 +8,12 @@ university_crime_count <- gss_data %>% filter(msvic < 900) %>% filter(eor_01c ==
 college_crime_count <- gss_data %>% filter(msvic < 900) %>% filter(eor_01b == 1) %>% 
   left_join(as.data.frame(list(msvic=msvic_num, msvic_label=msvic_labels))) %>%
   ggplot(aes(as.factor(msvic_label))) + geom_histogram(stat="count") + 
-  theme(plot.title = element_text(size=10), axis.text.x = element_text(angle = 60, hjust = 1)) + xlab("Crime") +
-  labs(title = "College Education Crime Count")
+  theme(plot.title = element_text(size=7), axis.text.x = element_text(angle = 60, hjust = 1)) + xlab("Crime") +
+  labs(title = "Trade School, College Education Crime Count")
 
 
 elementary_crime_count <- gss_data %>% filter(msvic < 900) %>% filter(eor_01a == 1) %>% 
   left_join(as.data.frame(list(msvic=msvic_num, msvic_label=msvic_labels))) %>%
   ggplot(aes(as.factor(msvic_label))) + geom_histogram(stat="count") + 
-  theme(plot.title = element_text(size=10), axis.text.x = element_text(angle = 60, hjust = 1)) + xlab("Crime") +
-  labs(title = "Elementary Education Crime Count")
+  theme(plot.title = element_text(size=6), axis.text.x = element_text(angle = 60, hjust = 1)) + xlab("Crime") +
+  labs(title = "Elementary/junior/high School Education Crime Count")
